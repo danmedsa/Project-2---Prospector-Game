c8e2ca487f26db3fe97ae5e81510acf69bb97562
danmedsa <daniel.medina-sada@ttu.edu>
2015-03-05 01:12:01 -0600

    So far

---
+++ Assets/__Scripts/Card.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card : MonoBehaviour {
+	public string suit;
+	public int rank;
+	public Color color = Color.black;
+	public string colS = "Black";
+
+	public List<GameObject> decoGOs = new List<GameObject> ();
+	public List<GameObject> pipGOs = new List<GameObject> ();
+
+	public GameObject back;
+	public CardDefinition def;
+
+	public bool faceUp{
+		get{
+			return(!back.activeSelf);
+		}
+		set{
+			back.SetActive(!value);
+		}
+	}
+}
+
+[System.Serializable]
+public class Decorator {
+
+	public string type;
+	public Vector3 loc;
+	public bool flip = false;
+	public float scale = 1f;
+}
+
+[System.Serializable]
+public class CardDefinition{
+	public string face;
+	public int rank;
+	public List<Decorator> pips = new List<Decorator> ();
+}
---
+++ Assets/__Scripts/Prospector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Prospector : MonoBehaviour {
+	static public Prospector S;
+	public Deck				 deck;
+	public TextAsset 		 deckXML;
+
+	// Use this for initialization
+	void Start () {
+		deck = GetComponent<Deck> ();
+		deck.InitDeck (deckXML.text);
+		Deck.Shuffle (ref deck.cards);
+	}
+	
+	// Update is called once per frame
+	void Awake () {
+		S = this;
+	}
+}
