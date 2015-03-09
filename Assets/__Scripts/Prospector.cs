50bfb09df335d13fcc4fc08a42751e27e58a60b2
danmedsa <daniel.medina-sada@ttu.edu>
2015-03-08 19:13:25 -0500

    up to pg. 593

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -7,11 +7,68 @@ public class Prospector : MonoBehaviour {
 	public Deck				 deck;
 	public TextAsset 		 deckXML;
 
+	public Layout			 layout;
+	public TextAsset		 layoutXML;
+	public Vector3			 layoutCenter;
+	public float			 xOffset = 3;
+	public float			 yOffset = -2.5f;
+	public Transform		 layoutAnchor;
+
+	public CardProspector	 target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
+
+	public List<CardProspector> drawPile;
+	
 	// Use this for initialization
 	void Start () {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
+
+		layout = GetComponent<Layout> ();
+		layout.ReadLayout (layoutXML.text);
+
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+		LayoutGame ();
+	}
+
+	CardProspector Draw(){
+		CardProspector cd = drawPile [0];
+		drawPile.RemoveAt(0);
+		return (cd);
+	}
+
+	void LayoutGame(){
+		if (layoutAnchor == null) {
+			GameObject tGO = new GameObject("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+
+		CardProspector cp;
+		foreach(SlotDef tSD in layout.slotDefs){
+			cp = Draw ();
+			cp.faceUp = tSD.faceUp;
+			cp.transform.parent = layoutAnchor;
+			cp.transform.localPosition = new Vector3(layout.multiplier.x*tSD.x, layout.multiplier.y * tSD.y,-tSD.layerID);
+			cp.layoutID = tSD.id;
+			cp.slotDef = tSD;
+			cp.state = CardState.tableau;
+
+			tableau.Add (cp);
+		}
+	}
+
+
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD){
+		List<CardProspector> lCP = new List<CardProspector> ();
+		CardProspector tCP;
+		foreach(Card tCD in lCD){
+			tCP = tCD as CardProspector;
+			lCP.Add (tCP);
+		}
+		return (lCP);
 	}
 	
 	// Update is called once per frame
